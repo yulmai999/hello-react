cc9503a499fcaeff60381a50d8d3ebc5ef19697a
yulmai999 <37208116+yulmai999@users.noreply.github.com>
2020-05-19 05:25:47 +0900

    5/19 state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
-import MyComponent from "./MyComponent";
+import Counter from "./Counter";
+import Say from "./Say";
 
 const App = () => {
-  return <MyComponent favoriteNumber={1}>리액트</MyComponent>;
+  return <Say />;
 };
 
 export default App;
